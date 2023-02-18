d0c59f1f5931a1cc56373586f5b1b122130ed523
Yeison Roa <alexandr.ortega@hotmail.com>
2023-02-17 20:06:54 -0500

    Typing Scrpit V1.7

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// ============= typing animated ==========
+
+var typed = new Typed('.typing', {
+    strings: ['', 'Web Designer', 'Web Developer', 'Data Science'],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
