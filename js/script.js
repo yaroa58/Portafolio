2624b5d15ccff3ef3967e62afdb448f8be709050
Yeison Roa <alexandr.ortega@hotmail.com>
2023-02-19 10:57:51 -0500

    adaptandyn

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // ============= typing animated ==========
 
 var typed = new Typed('.typing', {
-    strings: ['', 'Web Designer', 'Web Developer', 'Data Science'],
+    strings: ['', 'Web Designer', 'Web Developer', 'Data Science','Front End'],
     typeSpeed: 100,
     BackSpeed: 60,
     loop: true
--- js/sphere.js
+++ js/sphere.js
@@ -1,14 +1,13 @@
 
+const Texts = ['JavaScript', 'HTML', 'CSS', 'Java',
+    'MongoDB', 'Power BI', 'GIT', 'mySQL', 'REAC', 'Excel'];
+var tagcloud = TagCloud('.sphere', Texts, {
+    radius: 120,
+    maxSpeed: 'fast',
+    iniSpeed: 'fast',
+    direction: 135,
+    keep: true
+});
 
-const Texts = ['JavaScript', 'HTML', 'CSS','Java',
-               'MongoDB','Power BI','GIT','mySQL','REAC','Excel'];
- var tagcloud = TagCloud('.sphere', Texts, {
-  radius: 120,
-  maxSpeed: 'fast',
-  iniSpeed: 'fast',
-  direction: 135,
-  keep: true
-  });
-
-  var color = '#ec1839';
-  document.querySelector('.sphere').style.color = color;
+var color = '#ec1839';
+document.querySelector('.sphere').style.color = color;
